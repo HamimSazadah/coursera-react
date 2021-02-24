65694ee872fb67f0e9c0a5a331e591ab67ac3f3c
Hamim Sazadah <hamim.sazadah@gmail.com>
2021-02-24 15:11:20 +0700

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,20 +7,28 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 
 
-function RenderComments({comments, postComment, dishId}) {
+function RenderComments({ comments, postComment, dishId }) {
     if (typeof comments != 'undefined') {
         // console.log(comments, addComment, dishId);
         return (<ul style={{ listStyleType: 'none' }}>
-            {comments.map(n => {
-                return (<><li key={n.id}>{n.comment}</li>
-                    <li key={n.id + 'x'}>-- {`${n.author} ,${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(n.date)))}`}</li></>)
-            })
-            }
+            <Stagger in>
+                {comments.map((comment) => {
+                    return (
+                        <Fade in>
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        </Fade>
+                    );
+                })}
+            </Stagger>
             <CommentForm dishId={dishId} postComment={postComment} />
         </ul >)
     } else {
@@ -53,13 +61,19 @@ function RenderDish(props) {
     else if (props.dish != null)
         return (
             <>
-                <Card>
-                    <CardImg top src={baseUrl + dish2.image} alt={dish2.name} />
-                    <CardBody>
-                        <CardTitle>{dish2.name}</CardTitle>
-                        <CardText>{dish2.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform
+                    in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card>
+                        <CardImg top src={baseUrl + dish2.image} alt={dish2.name} />
+                        <CardBody>
+                            <CardTitle>{dish2.name}</CardTitle>
+                            <CardText>{dish2.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
                 <RenderComments comments={props.comments}
                     addComment={props.addComment}
                     dishId={props.dish.id}
